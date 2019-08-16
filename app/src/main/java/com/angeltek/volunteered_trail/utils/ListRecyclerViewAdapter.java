acdb53b7a565daa6769507f79761abd7d97e5839
amilmohd155 <amilmohd155@gmail.com>
2019-08-16 08:45:59 +0530

    commit

--- app/src/main/java/com/angeltek/volunteered_trail/utils/ListRecyclerViewAdapter.java
+++ app/src/main/java/com/angeltek/volunteered_trail/utils/ListRecyclerViewAdapter.java
@@ -7,25 +7,6 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-public class ListRecyclerViewAdapter extends RecyclerView.Adapter<ListRecyclerViewAdapter.ViewHolder> {
+public class ListRecyclerViewAdapter {
 
-    private ArrayList<T> mValues;
-    private Context context;
-    private int layoutResource;
-
-    @NonNull
-    @Override
-    public ListRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ListRecyclerViewAdapter.ViewHolder viewHolder, int i) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 }
